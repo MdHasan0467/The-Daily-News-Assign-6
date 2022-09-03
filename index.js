f2375940eecd3f7bfd1cd4a97a0a2bdce5c5b1c9
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-09-04 00:36:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// https://openapi.programming-hero.com/api/news/categories
+const loadCategories = () => {
+ const url = `https://openapi.programming-hero.com/api/news/categories`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategories(data.data.news_category))
+  .catch(error => console.log(error))
+}
+const displayCategories = category => {
+
+  const categoriesContainer = document.getElementById('categories-container');
+  categoriesContainer.innerHTML = '';
+  category.forEach(category => {
+    const categoryList = document.createElement('ul');
+    categoryList.classList.add('navbar-nav');
+    categoryList.innerHTML = `
+        <li class="nav-item p-4 bg-primary text-white">
+        <button  class=" btn btn-outline-warning px-1 py-2 border-0 nav-link fw-bold fs-5" href="#" onclick ="loadNews('${category.category_id}')">
+        ${category.category_name}
+        </button>
+        </li>                 
+      `;
+    categoriesContainer.appendChild(categoryList);
+  });
+  toggleSpinner(false);
+}
+
+document.getElementById('categories-container').addEventListener('click', function (){
+  toggleSpinner(true);
+  const spinnerLoader = document.getElementById('loading-spinner');
+  const spinnerLoaderValue = spinnerLoader.innerText;
+  loadCategories(spinnerLoaderValue);
+
+})
+
+
+const toggleSpinner = isLoading => {
+  const loaderSection = document.getElementById('loading-spinner');
+  if (isLoading) {
+    loaderSection.classList.remove('d-none');
+  }
+  else {
+    loaderSection.classList.add('d-none');
+  }
+}
+
+loadCategories();
+
---
+++ modal.js
@@ -0,0 +1,13 @@
+
+// const loadModal = (id) => {
+//  const url = `https://openapi.programming-hero.com/api/news/{id}`
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(data => displayModal(data))
+//   .catch(error => console.log(error))
+// }
+
+// const displayModal = modal => {
+
+  
+// }
